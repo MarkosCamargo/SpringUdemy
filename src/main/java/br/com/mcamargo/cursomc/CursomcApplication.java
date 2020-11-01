bb94cea34d38ea07b2a41811bd9834115e63e8ea
Marcos Camargo <markos_camargo@hotmail.com>
2020-11-01 18:04:24 -0300

    Produto e associacao muitos para muitos com cascade

--- src/main/java/br/com/mcamargo/cursomc/CursomcApplication.java
+++ src/main/java/br/com/mcamargo/cursomc/CursomcApplication.java
@@ -8,14 +8,16 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import br.com.mcamargo.cursomc.domain.Categoria;
+import br.com.mcamargo.cursomc.domain.Produto;
 import br.com.mcamargo.cursomc.repositories.CategoriaRepository;
+import br.com.mcamargo.cursomc.repositories.ProdutoRepository;
 
 @SpringBootApplication
 public class CursomcApplication implements CommandLineRunner {
 	
 	@Autowired
 	private CategoriaRepository catRepo;
-
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
 	}
@@ -24,7 +26,19 @@ public class CursomcApplication implements CommandLineRunner {
 	public void run(String... args) throws Exception {
 		Categoria oCat1 = new Categoria(null, "Informática");
 		Categoria oCat2 = new Categoria(null, "Escritório");
+		
+		Produto oProd1 = new Produto(null, "Computador", 2000.00);
+		Produto oProd2 = new Produto(null, "Impressora", 800.00);
+		Produto oProd3 = new Produto(null, "Mouse", 75.00);
+		
+		oProd1.addCategoria(oCat1);
+		oProd2.addCategoria(oCat1);
+		oProd3.addCategoria(oCat1);
+		
+		oProd2.addCategoria(oCat2);
+
 		this.catRepo.saveAll(Arrays.asList(oCat1, oCat2));
+
 	}
 
 }
